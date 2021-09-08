61b4c6d5a0ee72f6cd6ce9c91590570640b70bc4
5z3wczykj4kub <5z3wczykj4kub@gmail.com>
2021-09-08 16:10:07 +0200

    :sunglasses:

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const containerOuter = document.querySelector('.container__outer');
 const navigation = document.querySelector('.navigation__outer');
 const backdrop = document.querySelector('.backdrop');
 const wrapper = document.querySelector('.wrapper');
+const projects = document.querySelectorAll('.project');
+const modalBackdrop = document.querySelector('.modal-backdrop');
+
+const swiper = new Swiper('.swiper', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoHeight: true,
+});
 
 [...icons, backdrop].forEach((icon) => {
   icon.addEventListener('click', () => {
@@ -13,13 +26,15 @@ const wrapper = document.querySelector('.wrapper');
     backdrop.classList.toggle('backdrop--active');
 
     if (menu.classList.contains('menu__outer--open')) {
+      const modals = document.querySelectorAll('.modal.active');
+      modals.forEach((modal) => modal.classList.remove('active'));
+      modalBackdrop.classList.remove('active');
       document.body.style.overflow = 'hidden';
       wrapper.style.overflow = 'hidden';
       containerOuter.style.animation =
         'rotate 0.4s linear, float 3.5s 0.4s infinite';
     } else {
       document.body.style.overflow = 'visible';
-      // wrapper.style.overflow = 'visible';
       containerOuter.style.animation = 'derotate 0.4s linear';
     }
   });
@@ -35,4 +50,22 @@ backdrop.addEventListener('transitionend', () => {
   if (!backdrop.classList.contains('backdrop--active')) {
     backdrop.style.transform = 'translateX(-100%)';
   }
+});
+
+modalBackdrop.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach((modal) => modal.classList.remove('active'));
+  modalBackdrop.classList.remove('active');
+  document.body.style.overflow = 'visible';
+});
+
+projects.forEach((project) => {
+  project.addEventListener('click', () => {
+    if (project.classList.contains('mini-project')) return;
+    const { projectName } = project.dataset;
+    const modal = document.querySelector(`#${projectName}`);
+    modal.classList.add('active');
+    modalBackdrop.classList.add('active');
+    document.body.style.overflow = 'hidden';
+  });
 });
